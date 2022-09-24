9641b72da8217f30b9e1f8e8e4b247656a5cbc86
David Di Biase <ddibiase@MacBook-Pro-3.local>
2022-09-24 11:32:57 -0400

    Major updates and package name

--- README.md
+++ README.md
@@ -1,18 +1,19 @@
-# BuilderIO Field Types
+# Builder.io Type Extensions
 
-Builder Type Extensions is a set of helpful extension definitions for writing powerful well structured TypeScript codebases. The current library contains support for input field extraction.
+This package is meant to be a set of helpful TypeScript definitions for writing powerful well structured TypeScript codebases in Builder.io's CMS. The current library contains support for input field extraction.
 
 ## Component Input Type Helper
 
-This package exports types that make inferring [Builder's Component Input Types](https://www.builder.io/c/docs/custom-components-input-types). The `BuilderFields` and `GenerateItems` are used together to infer the input array into a properly structured list of properties for your component.
+This utility infers types based on Builder input field array [Builder.io's Component Input Types](https://www.builder.io/c/docs/custom-components-input-types). The `BuilderFields` and `GenerateItems` are used together to infer the input array into a properly structured list of properties for your component.
 
-Here's a sample input:
+Here's some sample input:
 
 ```ts
 const inputs = [
   {
     name: "name",
     type: "string",
+    required: true,
   },
   {
     name: "favouriteColours",
@@ -37,14 +38,14 @@ type InputProps = BuilderFields<GenerateItems<typeof inputs>>;
 > **Note**
 > The input must be cast as const or the inference will not work as expected.
 
-The above structured input fields would translate into the following type:
+The above fields would result into the following type:
 
 ```ts
 type InputProps = {
   name: string;
-  favouriteColours: {
-    name: string;
-    hex: string;
+  favouriteColours?: {
+    name?: string;
+    hex?: string;
   }[];
 };
 ```
@@ -62,3 +63,8 @@ function MyComponent(props: InputProps) => {
 
 `1.0.0` - Initial release
 `1.0.1` - Fixed packaging module issues
+`1.1.0` - Improved formatting, adding required fields, added string enums
+
+### Acknowledgments
+
+Thank you to a number of individuals such as trusktr (Joe Pea) and individuals in the SolidJS community. I'm not a TypeScript wizard in the slightest but I learned a _lot_ from pulling these types together.
--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
-// Type definitions for builder-type-extensions 2.10
+// Type definitions for builderio-type-extensions
 // Project: https://github.com/davedbase/builderio-field-types#readme
 // Definitions by: David Di Biase <https://github.com/davedbase>
-// TypeScript Version: 3.0
+// TypeScript Version: 4.0
 
 /**
  * Builder Type Extensions is a set of helpful extension definitions for writing
@@ -9,27 +9,49 @@
  * support for input field extraction.
  */
 
-declare type BuilderFields<T> = T extends object
-  ? T extends infer O ? { [K in keyof O]: BuilderFields<O[K]> } : never
-  : T;
-declare type GenerateItems<T extends readonly any[]> = {
-  [K in keyof T & `${bigint}` as T[K] extends { name: infer N extends string } 
-    ? N 
+type BuilderFields<T> = T extends object
+  ? T extends infer O
+    ? { [K in keyof O]: BuilderFields<O[K]> }
     : never
-  ]: 
-    T[K] extends { subFields: infer S extends readonly any[] } 
-      ? GenerateItems<S>[]
-      : T[K] extends { type: infer Type extends string } 
-        ? Type extends "string" | "reference" | "richText" | "color" | "email" | "longText" | "url"
-          ? string 
-          : Type extends "file"
-            ? T[K] extends { allowedFileTypes: infer FT extends readonly string[] }
-              ? FT[number]
-              : never
-            : Type extends "boolean"
-              ? boolean
-              : Type extends "date"
-                ? Date
-                : never
-        : never
-}
+  : T;
+
+type GenerateItem<T> = T extends { subFields: infer S extends readonly any[] }
+  ? GenerateItems<S>[]
+  : T extends { type: infer Type extends string }
+  ? Type extends "string"
+    ? T extends { enum: infer SE extends readonly string[] }
+      ? SE[number]
+      : string
+    : Type extends
+        | "file"
+        | "date"
+        | "richText"
+        | "color"
+        | "email"
+        | "longText"
+        | "url"
+    ? string
+    : Type extends "boolean"
+    ? boolean
+    : Type extends "number"
+    ? number
+    : Type extends "reference" | "object"
+    ? object
+    : never
+  : never;
+
+type GenerateItems<T extends readonly any[]> = {
+  [K in keyof T & `${bigint}` as T[K] extends {
+    name: infer N extends string;
+    required?: false;
+  }
+    ? N
+    : never]?: GenerateItem<T[K]>;
+} & {
+  [K in keyof T & `${bigint}` as T[K] extends {
+    name: infer N extends string;
+    required: true;
+  }
+    ? N
+    : never]: GenerateItem<T[K]>;
+};
